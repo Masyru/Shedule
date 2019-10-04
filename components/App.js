f3eb45da81ad6e3bd590db35995fd6c096a949ae
Maksim from Linux <mimoz200385@gmail.com>
2019-10-04 14:53:57 +1000

    Start point

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    let main = <div>Hello sber!</div>;
+
+    return(main);
+  }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+console.log("start")
+
+ReactDOM.render(<App />, document.getElementById("project"));
